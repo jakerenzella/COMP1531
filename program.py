c29b7152c30be8aa9e0eccdabef3a0f6696327fc
Jake Renzella <j@kerenzella.com>
2021-09-16 12:52:27 +1000

    change wording in suny

--- program.py
+++ program.py
@@ -4,7 +4,7 @@ def main():
     if weather == "raining":
         print("You should wear a raincoat when you go out")
     elif weather == "sunny":
-        print("You should wear a hat when you go out")
+        print("You should wear a hat when you go go out")
     else:
         print("Have a nice day!")
 
