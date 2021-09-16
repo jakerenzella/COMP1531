a041e405cedaf76a4d4860799ff1ea15b2f78750
Jake Renzella <j@kerenzella.com>
2021-09-16 12:49:49 +1000

    new: added weather program in python

---
+++ program.py
@@ -0,0 +1,11 @@
+def main():
+    weather = input("Enter the weather: ")
+
+    if weather == "raining":
+        print("You should wear a raincoat when you go out")
+    elif weather == "sunny":
+        print("You should wear a hat when you go out")
+    else:
+        print("Have a nice day!")
+
+main()
